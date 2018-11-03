64865c776d019bba8ca379c94712db0929448a8c
Sierra McCahon <sierrakatemitchell7@gmail.com>
2018-11-03 23:08:26 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,15 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid 
+  class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+  end 
+end
+  
